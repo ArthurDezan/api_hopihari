74283039f037d2a5fde99214e96b77c7421ab129
ArthurDezan <apdezan78@gmail.com>
2025-05-05 16:58:51 -0300

    database

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -3,14 +3,14 @@ const mysql = require('../myslq');
 exports.cadastrarUsuario = async (req, res) => {
     try {
         const resultado = await mysql.execute(
-            `insert into users (name, email, password) values (?, ?, ?)`,
-            [req.body.name, req.body.email, req.body.password]
+            `insert into users (first_name, last_name, email, password, birth_date, phone) values (?, ?, ?, ?, ?, ?)`,
+            [req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.birth_date, req.body.phone]
         );
-        return res.status(200).send({"Mensagem": "Usuário cadastrado com sucesso!", "Resultado": resultado});
+        return res.status(201).send({ "Mensagem": "Usuário cadastrado com sucesso!", "Resultado": resultado });
 
     } catch (error) {
-        return res.status(500).send({"Mensagem": error})
-    }
+        return res.status(500).send({ "Mensagem": error })
+    } 
 }
 
 exports.atualizarUsuario = async (req, res) => {
@@ -24,9 +24,9 @@ exports.atualizarUsuario = async (req, res) => {
             where id = ?;`,
             [req.body.name, req.body.email, req.body.password, idUsuario]
         );
-        return res.status(201).send({"Mensagem": "Usuário atualizado com sucesso!", "Resultado": resultado});
+        return res.status(201).send({ "Mensagem": "Usuário atualizado com sucesso!", "Resultado": resultado });
 
     } catch (error) {
-        return res.status(500).send({"Mensagem": error})
+        return res.status(500).send({ "Mensagem": error })
     }
 }
