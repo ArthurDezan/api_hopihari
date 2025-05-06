73baaca41eafb916c02750ccfdc13888c96a14f3
ArthurDezan <apdezan78@gmail.com>
2025-05-06 11:27:34 -0300

    login

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,10 +1,13 @@
 const mysql = require('../myslq');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.cadastrarUsuario = async (req, res) => {
     try {
+        const hash = await bcrypt.hash(req.body.password, 10);
         const resultado = await mysql.execute(
             `insert into users (first_name, last_name, email, password, birth_date, phone) values (?, ?, ?, ?, ?, ?)`,
-            [req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.birth_date, req.body.phone]
+            [req.body.first_name, req.body.last_name, req.body.email, hash, req.body.birth_date, req.body.phone]
         );
         return res.status(201).send({ "Mensagem": "Usuário cadastrado com sucesso!", "Resultado": resultado });
 
@@ -28,5 +31,25 @@ exports.atualizarUsuario = async (req, res) => {
 
     } catch (error) {
         return res.status(500).send({ "Mensagem": error })
+    }
+}
+
+exports.login = async (req, res) => {
+    try{
+        const usuario = await mysql.execute(`select * from users where email = ?`, [req.body.email]);
+        console.log(usuario);
+        if (usuario.length == 0){
+            return res.status(401).send({ "Mensagem": "Usuário não encontrado" });
+        }
+        const match = await bcrypt.compare(req.body.password, usuario[0].password);
+        if(!match){
+            return res.status(401).send({ "Mensagem": "Senha incorreta" });
+        }
+        const token = jwt.sign({ id: usuario[0].id, first_name: usuario[0].first_name, last_name: usuario[0].last_name, email: usuario[0].email, birth_date: usuario[0].birth_date}, 'senhajwt');
+
+        return res.status(200).send({ "Mensagem": "Usuário logado com sucesso!", "Token": token });
+
+    }catch(error){
+        return res.status(500).send({ "Error": error })
     }
 }
--- routes/usuarios.route.js
+++ routes/usuarios.route.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuarios.controller');
 
-router.post('/login', () => {
-    console.log('Rota de Login')
-});
+router.post('/login', usuariosController.login);
 router.put('/:id', usuariosController.atualizarUsuario);
 router.post('/cadastrar', usuariosController.cadastrarUsuario);
 
