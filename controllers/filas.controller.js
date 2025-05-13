bef9f3883f5142d695d06dd7df83366bec03f113
ArthurDezan <apdezan78@gmail.com>
2025-05-13 08:21:53 -0300

    saadff

--- controllers/filas.controller.js
+++ controllers/filas.controller.js
@@ -3,7 +3,7 @@ const mysql = require('../mysql');
 exports.entrarfila = async (req, res, next) => {
     try {
         const resultado = await mysql.execute(
-            `insert into users_has_atracoes (users_id, atracoes_id) values (?, ?)`,
+            `insert into lines (users_id, atracoes_id) values (?, ?)`,
             [res.locals.idUsuario, req.params.idRide]);
 
         return res.status(201).send({ "Mensagem": "Usuário entrou na fila com sucesso!", "Resultado": resultado });
